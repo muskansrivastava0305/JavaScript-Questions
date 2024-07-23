60c74ecd5fa8f74de60f167ad69badeace81929e
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-23 06:28:07 +0000

    commit

---
+++ 3.js
@@ -0,0 +1,34 @@
+
+for ( let i = 0; i <= 100 ; i += 2){
+       console.log(i);
+
+}
+
+
+//console.log(value);
+
+
+
+for (let i = 0; i <= 10 ; i++){
+    let num = ("7 * " + i + " = " + 7*i)
+    console.log(num);
+}
+
+
+//===sum of 10===
+
+let num = 0
+for (let i = 0 ; i <= 10 ; i++){
+    num += i
+}
+
+console.log(num);
+
+//=============calculate 10============
+
+let value = 1
+for (let i = 1 ; i<=10 ; i++){
+    value *= i
+}
+
+console.log(value);
